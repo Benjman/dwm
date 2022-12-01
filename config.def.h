2103293defbbd57a8186842dfaa7f87c1c21f6cd
Ben <benjamin.s.record@gmail.com>
2022-12-01 10:51:48 -0700

    disable formatting

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+// clang-format off
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -114,3 +116,4 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+// clang-format on
